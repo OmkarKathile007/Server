a1e89c736ed84e4b6817dd24489c54b2ed90d156
Omkar Kathile <omkarkathile007@gmail.com>
2024-07-27 14:18:24 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const express=require("express");
+const app=express()
+
+const port=3000
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Port Listening on ${port}`)
+})
